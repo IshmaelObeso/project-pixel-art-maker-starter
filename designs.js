d40adfefed9a4a1e85bff2b06dd25dcb7cd72dcc
Ishmael Obeso <i.ishmaelo.o@gmail.com>
2019-06-02 19:07:49 -0700

    Initial Commit
    Defined makeGrid() function

--- designs.js
+++ designs.js
@@ -2,9 +2,17 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+//makeGrid inputs = user inputs
 
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid(rows, columns) {
+  var pixelGrid = document.querySelector('#pixelCanvas'); //get correct object
+  var pixelGridRowConstructor = ''; //initialize constructors
+  var pixelGridColumnConstructor = '';
+  for (var i = 1; i <= rows; i++ ) { //build columns
+    pixelGridColumnConstructor += '<td></td>';
+  };
+  for (var i = 1; i <= columns; i++) { //stack rows
+    pixelGridRowConstructor += '<tr>' + pixelGridColumnConstructor + '</tr>';
+  };
+  pixelGrid.innerHTML = pixelGridRowConstructor; //insert grid to doc
 }
