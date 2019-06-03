3964d3e45cd2ae8e958436c232910a89baadd6a0
Ishmael Obeso <i.ishmaelo.o@gmail.com>
2019-06-03 12:45:51 -0700

    Connected colorSquares function to cell eventListeners. User can now change color of squares after picking color and clicking.

--- designs.js
+++ designs.js
@@ -4,28 +4,50 @@
 // When size is submitted by the user, call makeGrid()
 //makeGrid inputs = user inputs
 
+
 function makeGrid(event) {
+  //debug messages
+  console.log('makeGrid fired');
   event.preventDefault();
-  console.log('makeGrid firing'); //for debugging
-  var rows = Number(inputHeight.value);
-  var columns = Number(inputWidth.value);
-  var pixelGrid = document.querySelector('#pixelCanvas'); //get correct object
-  var pixelGridRowConstructor = ''; //initialize constructors
-  var pixelGridColumnConstructor = '';
+  //defining variables
+  var pixelGrid = document.querySelector('#pixelCanvas');
   pixelGrid.innerHTML = '';
-  for (var i = 1; i <= rows; i++ ) { //build columns
-    pixelGridColumnConstructor += '<td></td>';
-  }
-  for (var y = 1; y <= columns; y++) { //stack rows
-    pixelGridRowConstructor += '<tr>' + pixelGridColumnConstructor + '</tr>';
-  }
-  pixelGrid.innerHTML = pixelGridRowConstructor; //insert grid to doc
+  var rows = inputHeight.value;
+  var columns = inputWidth.value;
+  //initialize constructors
+  for (var y = 1; y <= rows; y++) { //stack row
+    //debug messages
+    console.log('loop1 firing');
+    var pixelGridRow = document.createElement('tr');
+    for (var i = 1; i <= columns; i++ ) { //build columns
+      //debug messages
+      console.log('loop2 firing');
+      var pixelGridColumn = document.createElement('td');
+      pixelGridColumn.addEventListener('click', colorSquares);
+      pixelGridRow.appendChild(pixelGridColumn);
+    }
+    pixelGrid.appendChild(pixelGridRow);
+  }//insert grid to doc
+
 }
+
 // User input monitoring for makeGrid function
 //define makeGrid inputs
 var inputHeight = document.querySelector('#inputHeight');
 var inputWidth = document.querySelector('#inputWidth');
 var form = document.querySelector('#sizePicker');
 
+
 //make eventListener to look for input updates
 form.addEventListener('submit', makeGrid);
+
+
+//User can change color of squares
+function colorSquares(event) {
+  console.log('colorSquares fired');
+  var colorInput = document.querySelector('#colorPicker');
+  var colorValue = colorPicker.value;
+  console.log(colorValue);
+  event.target.style.backgroundColor = colorValue;
+
+}
