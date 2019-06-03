17cdd7bb2706e1801d30a5fb63cacba696d7316f
Ishmael Obeso <i.ishmaelo.o@gmail.com>
2019-06-03 11:12:20 -0700

    changed makeGrid() in designs.js to take no parameters

--- designs.js
+++ designs.js
@@ -4,28 +4,27 @@
 // When size is submitted by the user, call makeGrid()
 //makeGrid inputs = user inputs
 
-function makeGrid(rows, columns) {
-  console.log('makeGrid firing');
+function makeGrid() {
+  console.log('makeGrid firing'); //for debugging
+  var rows = Number(inputHeight.value);
+  var columns = Number(inputWidth.value);
   var pixelGrid = document.querySelector('#pixelCanvas'); //get correct object
   var pixelGridRowConstructor = ''; //initialize constructors
   var pixelGridColumnConstructor = '';
   pixelGrid.innerHTML = '';
   for (var i = 1; i <= rows; i++ ) { //build columns
     pixelGridColumnConstructor += '<td></td>';
-  };
+  }
   for (var i = 1; i <= columns; i++) { //stack rows
     pixelGridRowConstructor += '<tr>' + pixelGridColumnConstructor + '</tr>';
-  };
+  }
   pixelGrid.innerHTML = pixelGridRowConstructor; //insert grid to doc
-};
+}
 // User input monitoring for makeGrid function
 //define makeGrid inputs
 var inputHeight = document.querySelector('#inputHeight');
 var inputWidth = document.querySelector('#inputWidth');
-var rows = Number(inputHeight.value);
-var columns = Number(inputWidth.value);
-//make eventListener to look for input updates
-inputHeight.addEventListener('change', makeGrid(rows, columns));
-inputWidth.addEventListener('change', makeGrid(rows, columns));
+var form = document.querySelector('#sizePicker');
 
-//makeGrid(2, 2);
+//make eventListener to look for input updates
+form.addEventListener('submit', makeGrid());
